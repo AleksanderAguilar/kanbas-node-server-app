8a93b4802ee9596cc5004e292520ac5b09e4119f
AleksanderAguilar <aleksanderaguilart@gmail.com>
2023-11-20 21:30:03 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+Lab5(app);
+Hello(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+
+// app.listen(4000);
+app.listen(process.env.PORT || 4000);
