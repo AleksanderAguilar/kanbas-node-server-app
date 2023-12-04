ace316d629cd3bed872acbda3484ce5e758412a3
AleksanderAguilar <aleksanderaguilart@gmail.com>
2023-12-04 14:42:24 -0500

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -1,19 +1,64 @@
 import express from "express";
+import "dotenv/config";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import cors from "cors";
+import session from "express-session";
+
+import mongoose from "mongoose";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
+
 import "dotenv/config";
 
+// const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/kanbas', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Check connection status
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB at localhost');
+});
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
+
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
+// app.listen(process.env.PORT || 4000);
