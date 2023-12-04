dce05980ac1e3b3b69923a60f5d658248d690b62
AleksanderAguilar <aleksanderaguilart@gmail.com>
2023-12-04 16:07:53 -0500

    asdf

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ import UserRoutes from "./users/routes.js";
 import "dotenv/config";
 
 // const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
-mongoose.connect('mongodb+srv://aleksanderaguilar:webdev123@cluster0.xg6z2gc.mongodb.net/kanbas?retryWrites=true&w=majority' ||'mongodb://127.0.0.1:27017/kanbas');
-
+// mongoose.connect('mongodb+srv://aleksanderaguilar:webdev123@cluster0.xg6z2gc.mongodb.net/kanbas?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/kanbas');
+mongoose.connect('mongodb://127.0.0.1:27017/kanbas')
 const app = express();
 
 app.use(express.json());
